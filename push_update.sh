4d062e2dcabb724381ce7b08dd7667b4877371d0
Alexander Bird <alexander.bird.42@gmail.com>
2016-03-04 23:21:40 +0000

    updated push_update.sh to not run if there are pending changes.

--- push_update.sh
+++ push_update.sh
@@ -1,1 +1,1 @@
-git submodule update --remote --recursive && git commit -a -m "Updated submodules" && git push origin HEAD
+git diff --quiet && git diff --cached --quiet && git submodule update --remote --recursive && git commit -a -m "Updated submodules" && git push origin HEAD
